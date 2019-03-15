768b177ec3ddeccd8decb6e51e2698e5206afd4b
Wynspeare <wynspeare@gmail.com>
2019-03-15 17:00:39 -0500

    Initial Django Commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/CarolineCessaro/Desktop/CP/Assessment-4/craigslist-backend/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
