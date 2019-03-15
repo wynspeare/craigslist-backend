1c2a1d26979c6577495ad4bb874195d0bab505d1
Wynspeare <wynspeare@gmail.com>
2019-03-15 17:05:08 -0500

    Added gitignore for venv

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/CarolineCessaro/Desktop/CP/Assessment-4/craigslist-backend/venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
